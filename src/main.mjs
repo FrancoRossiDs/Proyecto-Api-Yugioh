1f080efddaa4ab6b858393ef0c714e2c61c7e967
Franco Rossi <90211516+FrancoRossiDs@users.noreply.github.com>
2023-12-11 22:49:17 -0300

    AddComents.mjs

--- src/main.mjs
+++ src/main.mjs
@@ -1,12 +1,11 @@
-// main.mjs
-
+// Imports al main.mjs
 import { getCharacters } from './Cards/getCards.mjs';
 import { setupPagination } from './Pages/paginacion.mjs';
 import { cardsByType } from './Cards/typeCards.mjs';
 
 const main = document.querySelector('main');
 
-
+//Referencias
 const reset = document.querySelector('#resetButton');
 const monsterFilters = document.getElementById('monsterFilters');
 const spellFilters = document.getElementById('spellFilters');
@@ -22,16 +21,17 @@ const search = document.querySelector('#searchInput');
 const type = document.querySelector('#tipo');
 const footer = document.querySelector('footer');
 
+//Funcion para mostrar todas las cartas y mostrar los botones de cambiar entre páginas
 function inicio() {
     getCharacters();
     footer.style.display = 'block';
 }
-
+//Inicializacion del paginado inicial
 inicio();
 const { initPagination } = setupPagination(getCharacters);
 initPagination();
 
-
+//Array de los filtros inicializado como null
 const filters = {
     id: null,
     name: null,
@@ -41,8 +41,10 @@ const filters = {
     level: null,
 };
 
+//Escucha del evento del boton Reset, resetea el valor del buscador y de los filtros
 reset.addEventListener('click', () => {
     inicio();
+    //Reset de los filtros anteriores y justo a sus valores
     monsterFilters.style.display = 'none';
     spellFilters.style.display = 'none';
     trapFilters.style.display = 'none';
@@ -65,6 +67,7 @@ reset.addEventListener('click', () => {
     trap.value = 'null';
 })
 
+//Escucha del evento del cambio del id de la carta
 type.addEventListener('change', (e) => {
     if (e.target.value === "null") {
         footer.style.display = 'none';
@@ -77,6 +80,7 @@ type.addEventListener('change', (e) => {
     }
 });
 
+//Escucha del evento del cambio del filtro de id=tipo(Mosntruo, Magia, Trampa)
 tipoSelect.addEventListener('change', () => {
     const selectedType = tipoSelect.value;
 
@@ -88,6 +92,7 @@ tipoSelect.addEventListener('change', () => {
     trapFilters.style.display = 'none';
     reset.style.display = 'none';
 
+    //adaptacion del codigo ante cada opcion, se agregan mas filtros y se quitan los otros
     if (selectedType === 'monster') {
         footer.style.display = 'none';
         monsterFilters.style.display = 'block';
@@ -111,7 +116,7 @@ tipoSelect.addEventListener('change', () => {
     cardsByType(filters);
 });
 
-
+//Funcion para mostrar las cartas filtrados con sus filtros
 async function displayresult(result) {
     main.innerHTML = '';
     result.forEach(card => {
@@ -122,11 +127,12 @@ async function displayresult(result) {
     });
 }
 
-
+//Escucha del evento cuando se ingresa un valor a la barra de búsqueda y se presiona el botón de buscar
 search.addEventListener('blur', (e) => {
     if (e.target.value === "") {
         inicio();
     } else {
+        //Al ingresar un valor en la barra de busqueda se resetean los filtros 
         filters.attribute = null;
         filters.monsterClass = null;
         filters.level = null;
@@ -146,10 +152,12 @@ search.addEventListener('blur', (e) => {
     }
 });
 
+//Escucha del evento cuando se ingresa un valor a la barra de búsqueda y se presiona el Enter
 search.addEventListener('keydown', (e) => {
     if (e.target.value === "") {
         inicio();
     } else if (e.key === "Enter") {
+        //Al ingresar un valor en la barra de busqueda se resetean los filtros 
         filters.attribute = null;
         filters.monsterClass = null;
         filters.level = null;
@@ -172,25 +180,30 @@ search.addEventListener('keydown', (e) => {
     }
 });
 
+//Escucha del evento cuando se ingresa el valor del atributo de la carta
 atributo.addEventListener('change', (e) => {
     if (e.target.value === "null") {
+        //Se quita el footer y se actualiza el array de los filtros y se envia al filtro de las paginas
         footer.style.display = 'none';
         filters.attribute = null;
         cardsByType(filters);
     } else {
+         //Se quita el footer y se actualiza el array de los filtros y se envia al filtro de las paginas
         footer.style.display = 'none';
         filters.attribute = e.target.value;
         cardsByType(filters);
     }
 });
 
-
+//Escucha del evento cuando se ingresa el valor del tipo de la carta
 typo.addEventListener('change', (e) => {
     if (e.target.value === "null") {
+         //Se quita el footer y se actualiza el array de los filtros y se envia al filtro de las paginas
         footer.style.display = 'none';
         filters.type = null;
         cardsByType(filters);
     } else {
+         //Se quita el footer y se actualiza el array de los filtros y se envia al filtro de las paginas
         footer.style.display = 'none';
         filters.type = e.target.value;
         cardsByType(filters);
@@ -198,30 +211,37 @@ typo.addEventListener('change', (e) => {
 
 });
 
+//Escucha del evento cuando se ingresa el valor de la clase de la carta
 clase.addEventListener('change', (e) => {
     if (e.target.value === "null") {
+         //Se quita el footer y se actualiza el array de los filtros y se envia al filtro de las paginas
         footer.style.display = 'none';
         filters.monsterClass = null;
         cardsByType(filters);
     } else {
+         //Se quita el footer y se actualiza el array de los filtros y se envia al filtro de las paginas
         footer.style.display = 'none';
         filters.monsterClass = e.target.value;
         cardsByType(filters);
     }
 });
 
+//Escucha del evento cuando se ingresa el valor del nivel de la carta
 nivel.addEventListener('change', (e) => {
     if (e.target.value === "null") {
+         //Se quita el footer y se actualiza el array de los filtros y se envia al filtro de las paginas
         footer.style.display = 'none';
         filters.level = null;
         cardsByType(filters);
     } else {
+         //Se quita el footer y se actualiza el array de los filtros y se envia al filtro de las paginas
         footer.style.display = 'none';
         filters.level = e.target.value;
         cardsByType(filters);
     }
 });
 
+//Escucha del evento cuando se ingresa el subfiltro de las cartas mágicas
 spell.addEventListener('change', (e) => {
     console.log(e.target.value);
     if (e.target.value === "null") {
@@ -229,21 +249,27 @@ spell.addEventListener('change', (e) => {
         filters.type = null;
         cardsByType(filters);
     } else {
+         //Se quita el footer y se actualiza el array de los filtros y se envia al filtro de las paginas
         footer.style.display = 'none';
         filters.type = e.target.value;
         cardsByType(filters);
     }
 });
 
+//Escucha del evento cuando se ingresa el subfiltro de las cartas trampa
 trap.addEventListener('change', (e) => {
     if (e.target.value === "null") {
-        filters.trapType = null;
-    } else {
         footer.style.display = 'none';
-        filters.trapType = e.target.value;
+        filters.type = null;
+        cardsByType(filters);
+    } else {
+         //Se quita el footer y se actualiza el array de los filtros y se envia al filtro de las paginas
+        footer.style.display = 'none';
+        filters.type = e.target.value;
+        cardsByType(filters);
     }
     cardsByType(filters);
 });
 
-
+//Export de la funcion para imprimir las cartas para recibir los filtros
 export { displayresult }
